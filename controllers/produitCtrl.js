89b09ea0506774b3fb7c52ba6ca5ad587f25071b
AchaUmbaPretre <achandambi@gmail.com>
2023-12-19 14:36:27 +0100

    commit

--- controllers/produitCtrl.js
+++ controllers/produitCtrl.js
@@ -74,3 +74,366 @@ exports.getProduitRecement = (req, res) => {
       return res.status(200).json(data);
     });
   };
+
+  exports.postProduit = (req, res) => {
+    const q = 'INSERT INTO produits(`nom_produit`,`id_categorie`,`id_marque`,`id_matiere`,`actif`,`date_entrant`,`date_MisAjour`,`id_cible`, `prix`) VALUES(?)';
+    const values = [
+        req.body.nom_produit,
+        req.body.id_categorie,
+        req.body.id_marque,
+        req.body.id_matiere,
+        req.body.actif,
+        req.body.date_entrant,
+        req.body.date_MisAjour,
+        req.body.id_cible
+    ]
+
+    db.query(q, [values], (error,data)=>{
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        return res.json({ message: 'Processus réussi'});
+        }
+      })
+}
+exports.deleteProduit = (req, res) => {
+    const {id} = req.params;
+    const q = "UPDATE produits SET est_supprime = 1 WHERE id = ?";
+  
+    db.query(q, [id], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  };
+
+exports.putProduit = (req, res) => {
+    const productId = req.params.id;
+  
+    const q = 'UPDATE produits SET nom_produit = ?, couleur = ?, matiere = ?, marque = ?, pointure = ?, categorie = ?, description = ?, img = ? WHERE id = ?';
+    const values = [
+      req.body.nom_produit,
+      req.body.couleur,
+      req.body.matiere,
+      req.body.marque,
+      req.body.pointure,
+      req.body.categorie,
+      req.body.description,
+      req.body.img,
+      productId 
+    ];
+  
+    db.query(q, values, (error, data) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json(error);
+      } else {
+        const updatedRows = data.affectedRows;
+  
+        if (updatedRows === 0) {
+          return res.status(404).json({ error: 'Produit non trouvé' });
+        }
+  
+        const shoeQ = 'UPDATE chaussures SET quantite_stock = ?, emplacement = ?, prix = ? WHERE produit_id = ?';
+        const shoeValues = [
+          req.body.quantite_stock,
+          req.body.emplacement,
+          req.body.prix,
+          productId 
+        ];
+  
+        db.query(shoeQ, shoeValues, (error, data) => {
+          if (error) {
+            console.error('Erreur lors de la mise à jour des données de la chaussure :', error);
+            res.status(500).json({ error: 'Erreur lors de la mise à jour des données de la chaussure' });
+            return;
+          }
+  
+          return res.json({ message: 'Produit mis à jour avec succès' });
+        });
+      }
+    });
+  };
+
+  //Couleur
+exports.getCouleur = (req, res) => {
+
+  const q = "SELECT * FROM couleur";
+   
+  db.query(q, (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+//Categorie
+exports.getCategorie = (req, res) => {
+
+  const q = "SELECT * FROM categorie INNER JOIN famille ON categorie.id_famille = famille.id_famille";
+   
+  db.query(q, (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+exports.getCategorieOne = (req, res) => {
+  const {id} = req.params;
+
+  const q = "SELECT * FROM categorie WHERE id = ?";
+   
+  db.query(q, id, (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+exports.postCategorie = (req, res) => {
+  const q = 'INSERT INTO categorie(`nom_categorie`,`id_famille`) VALUES (?,?)';
+
+  const values = [
+    req.body.nom_categorie,
+    req.body.id_famille
+  ]
+
+  db.query(q, values, (error, data) => {
+    if (error) {
+      res.status(500).json(error);
+      console.log(error);
+    } else {
+      res.json('Processus réussi');
+    }
+  });
+};
+
+exports.deleteCategorie = (req, res) => {
+  const {id} = req.params;
+  const q = "DELETE FROM categorie WHERE id = ?"
+
+  db.query(q, [id], (err, data)=>{
+      if (err) return res.send(err);
+    return res.json(data);
+  })
+};
+
+exports.putCategorie = (req, res) => {
+  const {id} = req.params;
+const q = "UPDATE categorie SET `nom_categorie`= ? WHERE id = ?"
+const { nom_categorie } = req.body;
+
+
+db.query(q, [nom_categorie,id], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+}
+
+
+//Emplacement
+exports.getEmplacement = (req, res) => {
+
+  const q = "SELECT * FROM emplacement";
+   
+  db.query(q, (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+exports.getEmplacementOne = (req, res) => {
+  const {id} = req.params;
+
+  const q = "SELECT * FROM emplacement id =?";
+   
+  db.query(q, id,(error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+exports.postEmplacement = (req, res) => {
+  const q = 'INSERT INTO emplacement(`nom`, `capacite`) VALUES(?,?)';
+
+  const values = [
+      req.body.nom,
+      req.body.capacite
+  ]
+  db.query(q, values, (error, data) => {
+    if (error) {
+      res.status(500).json(error);
+      console.log(error);
+    } else {
+      res.json('Processus réussi');
+    }
+  });
+};
+
+exports.deleteEmplacement = (req, res) => {
+  const {id} = req.params;
+  const q = "DELETE FROM emplacement WHERE id = ?"
+
+  db.query(q, [id], (err, data)=>{
+      if (err) return res.send(err);
+    return res.json(data);
+  })
+};
+
+exports.putEmplacement = (req, res) => {
+  const { id } = req.params;
+
+  const q = "UPDATE emplacement SET `nom` = ?, `capacite` = ? WHERE id = ?";
+  const values = [
+    req.body.nom,
+    req.body.capacite
+  ];
+
+  db.query(q, [...values, id], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+};
+
+//Matiere
+exports.getMatiere = (req, res) => {
+
+  const q = "SELECT * FROM matiere WHERE est_supprime = 0";
+   
+  db.query(q, (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+exports.getMatiereOne = (req, res) => {
+  const {id} = req.params;
+
+  const q = "SELECT * FROM matiere WHERE est_supprime = 0 AND id = ?";
+   
+  db.query(q,[id], (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+exports.postMatiere = (req, res) => {
+  const q = 'INSERT INTO matiere(`nom`) VALUES(?)';
+
+  const values = [
+      req.body.nom,
+  ]
+  db.query(q, values, (error, data) => {
+    if (error) {
+      res.status(500).json(error);
+      console.log(error);
+    } else {
+      res.json('Processus réussi');
+    }
+  });
+};
+
+exports.deleteMatiere = (req, res) => {
+  const { id } = req.params;
+  const q = "DELETE FROM matiere WHERE id = ?";
+
+  db.query(q, [id], (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json(err);
+    }
+    return res.json(data);
+  });
+};
+
+exports.putMatiere = (req, res) => {
+  const { id } = req.params;
+  const q = "UPDATE matiere SET `nom` = ? WHERE id = ?";
+  const values = [
+    req.body.nom,
+    id
+  ];
+
+  db.query(q, values, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json(err);
+    }
+    return res.json(data);
+  });
+};
+
+//Marque
+exports.getMarque = (req, res) => {
+
+  const q = "SELECT * FROM marque ";
+   
+  db.query(q, (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+
+exports.getMarqueOne = (req, res) => {
+  const {id} = req.params;
+  const q = "SELECT * FROM marque WHERE id = ?";
+   
+  db.query(q, id, (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
+
+exports.postMarque = (req, res) => {
+  const q = 'INSERT INTO marque(`nom`) VALUES(?)';
+
+  const values = [
+      req.body.nom,
+  ]
+  db.query(q, values, (error, data) => {
+    if (error) {
+      res.status(500).json(error);
+      console.log(error);
+    } else {
+      res.json('Processus réussi');
+    }
+  });
+};
+
+exports.deleteMarque = (req, res) => {
+  const { id } = req.params;
+  const q = "DELETE FROM marque WHERE id = ?";
+
+  db.query(q, [id], (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json(err);
+    }
+    return res.json(data);
+  });
+};
+
+exports.putMarque = (req, res) => {
+  const { id } = req.params;
+  const q = "UPDATE marque SET `nom` = ? WHERE id = ?";
+  const values = [
+    req.body.nom,
+    id
+  ];
+
+  db.query(q, values, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json(err);
+    }
+    return res.json(data);
+  });
+};
+//famille
+exports.getFamille = (req, res) => {
+
+  const q = "SELECT * FROM famille";
+   
+  db.query(q, (error, data) => {
+      if (error) res.status(500).send(error);
+      return res.status(200).json(data);
+  });
+}
--- routes/produitRoutes.js
+++ routes/produitRoutes.js
@@ -1,7 +1,36 @@
 const express = require("express");
-const { getProduit } = require("../controllers/produitCtrl.js");
+const { getProduit, postProduit, getCouleur, getCategorie, getCategorieOne, postCategorie, deleteCategorie, putCategorie, getEmplacement, getEmplacementOne, postEmplacement, deleteEmplacement, putEmplacement, getMatiere, getMatiereOne, postMatiere, deleteMatiere, putMatiere, getMarque, getMarqueOne, postMarque, deleteMarque, putMarque, getFamille } = require("../controllers/produitCtrl.js");
 const router = express.Router();
 
 router.get('/', getProduit)
+router.post('/produit', postProduit)
+
+router.get('/couleur', getCouleur);
+
+router.get('/categorie', getCategorie);
+router.get('/categorieOne/:id', getCategorieOne);
+router.post('/categorie', postCategorie)
+router.delete('/categorie/:id', deleteCategorie)
+router.put('/categoriePut/:id', putCategorie)
+
+router.get('/emplacement', getEmplacement)
+router.get('/emplacementOne/:id', getEmplacementOne)
+router.post('/emplacement', postEmplacement)
+router.delete('/emplacement/:id', deleteEmplacement)
+router.put('/emplacementPut/:id', putEmplacement)
+
+router.get('/matiere', getMatiere)
+router.get('/matiereOne/:id', getMatiereOne)
+router.post('/matiere', postMatiere)
+router.delete('/matiere/:id', deleteMatiere)
+router.put('/matiereUpdate/:id', putMatiere)
+
+router.get('/marque', getMarque)
+router.get('/marqueOne/:id', getMarqueOne)
+router.post('/marque', postMarque)
+router.delete('/marque/:id', deleteMarque)
+router.put('/marque/:id', putMarque)
+
+router.get('/famille', getFamille)
 
 module.exports = router;
