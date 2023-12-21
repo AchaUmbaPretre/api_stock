1c9c62bb93061dbd2b4b326310ef4652df5a3fa2
AchaUmbaPretre <achandambi@gmail.com>
2023-12-21 12:22:42 +0100

    commit

--- controllers/produitCtrl.js
+++ controllers/produitCtrl.js
@@ -33,11 +33,15 @@ exports.getProduit = (req, res) => {
 exports.getProduitOne = (req,res) => {
 
   const {id} = req.params;
-  const q = `SELECT produit.*, image_produit.image, categorie.nom_categorie, marque.nom AS nom_marque, matiere.nom_matiere, famille.nom AS nom_famille FROM produit
+  const q = `SELECT produit.*, image_produit.image, categorie.nom_categorie,
+                marque.nom AS nom_marque, matiere.nom_matiere,
+                famille.nom AS nom_famille, cible.nom_cible
+              FROM produit
               INNER JOIN categorie ON produit.id_categorie = categorie.id_categorie
               INNER JOIN marque ON produit.id_marque = marque.id_marque
               INNER JOIN matiere ON produit.id_matiere = matiere.id_matiere
               INNER JOIN famille ON categorie.id_famille = famille.id_famille
+              INNER JOIN cible ON produit.id_cible = cible.id_cible
               INNER JOIN image_produit ON produit.id_produit = image_produit.id_produit
             WHERE est_supprime = 0 AND produit.id_produit = ?`;
 
