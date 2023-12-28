0642cba3eef342a1f51ebfc009bc1b92b46175bb
AchaUmbaPretre <achandambi@gmail.com>
2023-12-28 15:47:03 +0100

    commit

--- routes/commandeRoutes.js
+++ routes/commandeRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const { getDemandeCommande, getDemandeCommandeCount, postDemandeCommande, deleteDemandeCommande } = require("../controllers/commandeCtrl");
-const { getProduit } = require("../controllers/produitCtrl");
 const router = express.Router();
 
 
 
 router.get('/', getDemandeCommande)
 router.get('/demande-commande', getDemandeCommandeCount)
-router.post('/client', postDemandeCommande)
-router.delete('/clientDelete/:id', deleteDemandeCommande)
+router.post('/demande-commande', postDemandeCommande)
+router.delete('/demande-commande/:id', deleteDemandeCommande)
 
 module.exports = router;
