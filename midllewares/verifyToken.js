bf17c0c594f00c8f1ad91fcec2325ba5367b9649
AchaUmbaPretre <achandambi@gmail.com>
2024-01-03 16:29:39 +0100

    commit

---
+++ midllewares/verifyToken.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Le jeton n'est pas valide !");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("Vous n'êtes pas authentifié !");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Vous n’êtes pas autorisé à faire ça !");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Vous n’êtes pas autorisé à faire ça !");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
