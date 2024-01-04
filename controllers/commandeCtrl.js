c72503000d4d71cff4ece1a927dcc48f55894933
AchaUmbaPretre <achandambi@gmail.com>
2024-01-04 17:10:08 +0100

    commit

--- controllers/commandeCtrl.js
+++ controllers/commandeCtrl.js
@@ -24,27 +24,28 @@ exports.getDemandeCommande = (req, res) => {
 }
 
 exports.postDemandeCommande = (req, res) => {
-    const q = 'INSERT INTO detail_commande(`id_commande`,`id_varianteProduit`, `id_client`, `prix`, `statut_demande`, `statut_demande`,`description`, `quantite`,`user_cr`) VALUES(?)';
-  
-    const values = [
-        req.body.id_commande,
-        req.body.id_varianteProduit,
-        req.body.id_client,
-        req.body.id_prix,
-        req.body.statut_demande,
-        req.body.description,
-        req.body.quantite
-    ]
-    db.query(q, values, (error, data) => {
-      if (error) {
-        res.status(500).json(error);
-        console.log(error);
-      } else {
-        res.json('Processus réussi');
-      }
-    });
-  };
-  
+  const q = 'INSERT INTO detail_commande(`id_commande`, `id_varianteProduit`, `id_client`, `prix`, `statut_demande`, `description`, `quantite`, `user_cr`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+
+  const values = [
+    req.body.id_commande,
+    req.body.id_varianteProduit,
+    req.body.id_client,
+    req.body.prix,
+    req.body.statut_demande,
+    req.body.description,
+    req.body.quantite,
+    req.body.user_cr
+  ];
+
+  db.query(q, values, (error, data) => {
+    if (error) {
+      res.status(500).json(error);
+      console.log(error);
+    } else {
+      res.json('Processus réussi');
+    }
+  });
+};
 
 exports.deleteDemandeCommande = (req, res) => {
     const {id} = req.params;
@@ -59,7 +60,10 @@ exports.deleteDemandeCommande = (req, res) => {
 //Commande
 
 exports.getCommande = (req, res) => {
-  const q = "SELECT * FROM commande WHERE est_supprime = 0";
+  const q = `SELECT *
+            FROM commande
+            INNER JOIN client ON commande.id_client = client.id
+            WHERE commande.est_supprime = 0;`;
    
   db.query(q, (error, data) => {
       if (error) res.status(500).send(error);
@@ -78,17 +82,17 @@ exports.getCommandeCount = (req, res) => {
 }
 
 exports.postCommande = (req, res) => {
-    const q = 'INSERT INTO commande(`id_client`, `statut`, `id_livraison`, `id_paiement`, `user_cr`, `id_shop`, `paye`, `retour`) VALUES(?,?,?,?,?,?,?,?,?)';
-  
+    const q = 'INSERT INTO commande(`id_client`, `statut`, `id_livraison`, `id_paiement`, `user_cr`, `id_shop`, `paye`, `retour`) VALUES(?,?,?,?,?,?,?,?)';
+    const paye = req.body.paye !== undefined ? req.body.paye : 0;
     const values = [
         req.body.id_client,
         req.body.statut,
-        req.body.id_livraison,
-        req.body.id_paiement,
-        req.body.user_cr,
-        req.body.id_shop,
-        req.body.paye,
-        req.body.retour
+        req.body.id_livraison || 0,
+        req.body.id_paiement || 0,
+        req.body.user_cr || 0,
+        req.body.id_shop || 1,
+        paye,
+        req.body.retour 
     ]
     db.query(q, values, (error, data) => {
       if (error) {
@@ -109,4 +113,16 @@ exports.deleteCommande = (req, res) => {
         if (err) return res.send(err);
       return res.json(data);
     })
+  }
+
+  //Status
+
+  exports.getStatus = (req, res) => {
+    const q = `SELECT *
+              FROM statut`;
+     
+    db.query(q, (error, data) => {
+        if (error) res.status(500).send(error);
+        return res.status(200).json(data);
+    });
   }
--- routes/commandeRoutes.js
+++ routes/commandeRoutes.js
@@ -1,16 +1,20 @@
 const express = require("express");
-const { getDemandeCommande, getDemandeCommandeCount, postDemandeCommande, deleteDemandeCommande, getCommandeCount, getCommande, postCommande, deleteCommande } = require("../controllers/commandeCtrl");
+const { getDemandeCommande, getDemandeCommandeCount, postDemandeCommande, deleteDemandeCommande, getCommandeCount, getCommande, postCommande, deleteCommande, getStatus } = require("../controllers/commandeCtrl");
 const router = express.Router();
 
 //commande
 router.get('/', getCommande)
 router.get('/commandeCount', getCommandeCount)
-router.post('/commande', postCommande)
+router.post('/commandePost', postCommande)
 router.delete('/commande/:id', deleteCommande)
+
+//Status
+router.get('/statut', getStatus)
+
 
 //Taille Commande
 router.get('/detail-commande', getDemandeCommande)
-router.get('/detail-commande', getDemandeCommandeCount)
+router.get('/detail-commandeCount', getDemandeCommandeCount)
 router.post('/detail-commande', postDemandeCommande)
 router.delete('/detail-commande/:id', deleteDemandeCommande)
 
