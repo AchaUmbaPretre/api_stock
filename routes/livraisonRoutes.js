b3375235c77ec3a3d6e3a1ffbf98f29d390079cf
AchaUmbaPretre <achandambi@gmail.com>
2024-01-05 14:25:24 +0100

    commit

--- routes/livraisonRoutes.js
+++ routes/livraisonRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getLivraison, getLivraisonOne, postLivraison, deleteLivraison } = require("../controllers/livraisonCtrl");
+const { getLivraison, getLivraisonOne, postLivraison, deleteLivraison, getLivraisonDetail, getLivraisonDetailOne, postLivraisonDetail, deleteLivraisonDetail } = require("../controllers/livraisonCtrl");
 const router = express.Router();
 
 
@@ -8,5 +8,12 @@ router.get("/", getLivraison);
 router.get("/livraisonOne/:id", getLivraisonOne);
 router.post("/", postLivraison);
 router.delete("/livraisonDelete/:id", deleteLivraison);
+
+//Detail livraison
+router.get("/livraisonDetail", getLivraisonDetail);
+router.get("/livraisonDetailOne/:id", getLivraisonDetailOne);
+router.post("/livraisonDetail", postLivraisonDetail);
+router.delete("/livraisonDelete/:id", deleteLivraisonDetail);
+
 
 module.exports = router;
