6e88fc127565ec066bcfe150d0626334bfa03e16
AchaUmbaPretre <achandambi@gmail.com>
2024-02-14 14:33:08 +0100

    commit

--- controllers/venteCtrl.js
+++ controllers/venteCtrl.js
@@ -281,22 +281,23 @@ exports.getRapportVente = (req, res) => {
   const { start_date, end_date, marque_id } = req.query;
 
   let q = `
-    SELECT
-      m.id_marque,
-      SUM(v.quantite) AS quantite_vendue,
-      SUM(v.prix_unitaire * v.quantite) AS montant_vendu,
-      vp.stock AS quantite_en_stock,
-      vp.img,
-      taille.taille,
-      m.nom AS nom_marque,
-      categorie.nom_categorie
-    FROM vente v
-    INNER JOIN varianteproduit vp ON v.id_detail_commande = vp.id_varianteProduit
-    INNER JOIN produit p ON vp.id_produit = p.id_produit
-    INNER JOIN marque m ON p.id_marque = m.id_marque
-    INNER JOIN taille ON vp.id_taille = taille.id_taille
-    INNER JOIN categorie ON p.id_categorie = categorie.id_categorie
-    WHERE v.est_supprime = 0
+  SELECT
+  m.id_marque,
+  SUM(v.quantite) AS quantite_vendue,
+  SUM(v.prix_unitaire * v.quantite) AS montant_vendu,
+  vp.stock AS quantite_en_stock,
+  vp.img,
+  taille.taille,
+  m.nom AS nom_marque,
+  categorie.nom_categorie
+FROM vente v
+INNER JOIN detail_commande ON v.id_detail_commande = detail_commande.id_detail
+INNER JOIN varianteproduit vp ON detail_commande.id_varianteProduit = vp.id_varianteProduit
+INNER JOIN produit p ON vp.id_produit = p.id_produit
+INNER JOIN marque m ON p.id_marque = m.id_marque
+INNER JOIN taille ON vp.id_taille = taille.id_taille
+INNER JOIN categorie ON p.id_categorie = categorie.id_categorie
+WHERE v.est_supprime = 0
       ${start_date ? `AND v.date_vente >= '${start_date}'` : ''}
       ${end_date ? `AND v.date_vente <= '${end_date}'` : ''}
   `;
@@ -305,12 +306,12 @@ exports.getRapportVente = (req, res) => {
     q += ` AND m.id_marque = ${marque_id}`;
   }
 
-  q += ' GROUP BY taille.taille';
+  q += ' GROUP BY m.id_marque';
 
   try {
     db.query(q, (error, data) => {
       if (error) {
-        return res.status(500).json({ error: error.message });
+        return res.status(500).json({ error: 'Tous les champs sont requis' });
       }
 
       return res.status(200).json(data);
